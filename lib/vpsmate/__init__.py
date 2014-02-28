34fd8f3ae0b9adbe7d73a399a3b71f9894d5cd61
zhangliyuanos@gmail.com <zhangliyuanos@gmail.com@096ae5b4-4869-0402-8ad4-25095c8241bb>
2014-02-28 05:09:44 +0000

    delete old lib

--- lib/vpsmate/__init__.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf-8 -*-
-#
-# Copyright (c) 2012, VPSMate development team
-# All rights reserved.
-#
-# VPSMate is distributed under the terms of the (new) BSD License.
-# The full license can be found in 'LICENSE.txt'.
-
-"""The VPSMate web server and tools."""
