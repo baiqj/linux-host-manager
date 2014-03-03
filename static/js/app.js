f99acee7f1c14968a6465cec4c1347db09c9e68b
zhangliyuanos@gmail.com <zhangliyuanos@gmail.com@096ae5b4-4869-0402-8ad4-25095c8241bb>
2014-03-03 03:04:34 +0000

    upload static

---
+++ static/js/app.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/* App Module */
+
+var cloudApp = angular.module('cloudApp', [
+  'ngRoute',
+  'cloudAppControllers',
+  'cloudAppDirectives',
+  'cloudAppServices',
+  'ui.bootstrap',
+]);
+
+cloudApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'loginCtrl'
+      }). 
+      when('/main', {
+		    templateUrl: 'partials/main.html',
+		    controller: 'mainCtrl'
+		  }).
+      when('/OneKey', {
+        templateUrl: 'partials/OneKey/index.html',
+        controller: 'OneKeyCtrl'
+      }).
+      when('/sites/create', {
+		    templateUrl: 'partials/OneKey/create.html',
+		    controller:	'siteCreateCtrl'
+      }).
+      when('/sites/creating', {
+        templateUrl: 'partials/OneKey/creating.html',
+        controller: 'siteCreatingCtrl'
+      }).
+      when('/sites/lnmp', {
+        templateUrl: 'partials/OneKey/lnmp.html',
+        controller: 'lnmpCtrl'
+      }).
+      when('/sites/onekey/lnmp-start', {
+        templateUrl: 'partials/OneKey/lnmp-processing.html',
+        controller: 'lnmpCtrl'
+      }).
+      when('/sites/lamp', {
+        templateUrl: 'partials/OneKey/lamp.html',
+        controller: 'lampCtrl'
+      }).
+      when('/sites/onekey/lamp-start', {
+        templateUrl: 'partials/OneKey/lamp-processing.html',
+        controller: 'lampCtrl'
+      }).
+      otherwise({
+        redirectTo: '/login'
+      });
+  }]).
+run(['$rootScope', '$location', '$timeout',function($rootScope, $location,$timeout) {
+  $rootScope.isLogin = false;
+
+  // from server get notice itmes and set variable items;
+  $rootScope.items = [ {title:"second item",href:"dddd"},{title:"thire item",href:"22222"},{title:"forth item",href:"ddddd"} ];
+
+  $rootScope.$on('$locationChangeSuccess', function () {
+      var currentPath = $location.path();
+
+      if(currentPath.indexOf("login") >= 0 ){
+          console.log('$locationChangeSuccess changed!', new Date());
+          $rootScope.isLogin = false;
+        } else {
+          $rootScope.isLogin = true;
+          console.log("1234567");
+        } 
+      });
+
+  $rootScope.htmlTitle = "首页";
+
+ /* var testTimeout = function(){
+    console.log("test for timeout use.");
+  }
+
+  $timeout(function(){
+    testTimeout.call();
+  },1);*/
+
+}]);
---
+++ static/js/controllers.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/* Controllers */
+
+var cloudAppControllers = angular.module('cloudAppControllers', ['ui.bootstrap']);
+
+cloudAppControllers.controller('loginCtrl', ['$scope',  'Request',
+  function($scope, Request) {
+  
+	/* code section */
+	 //$scope.bookname = 'bookname-value';
+  }]);
+
+cloudAppControllers.controller('mainCtrl', ['$rootScope','$scope',
+  function($rootScope,$scope){
+	/* custom code section */
+  $rootScope.htmlTitle = "首页";
+
+  // get system base information from server
+  $scope.sys_info = {os: 'RedHat', time: '12:23:56',run_time: '27:34:21',sys_Load: '56%', CPU:'core i5',Memory:'4096M',IP:'211.45.233.14'};
+  }]);
+
+cloudAppControllers.controller('OneKeyCtrl', ['$rootScope','$scope',
+  function($rootScope,$scope) {
+  /* custom code section */
+  $rootScope.htmlTitle = "一键通";
+  
+  }]);
+
+cloudAppControllers.controller('siteCreateCtrl', ['$rootScope','$scope','$location',
+  function($rootScope,$scope,$location) {
+  /* custom code section */
+  $rootScope.htmlTitle = "一键通";
+
+  //from server get the version information and set the variable in the below
+  $scope.http_versions  = ['1.2.3','3.4.5','5.1.6'];
+  $scope.mysql_versions = ['1.2.3','3.4.5','5.1.6'];
+  $scope.php_versions   = ['1.2.3','3.4.5','5.1.6'];
+
+  $scope.http_version = 0;
+  $scope.mysql_version = 0;
+  $scope.php_version = 0;
+  $scope.charsetOpt = "utf-8";
+
+  $scope.submit = function(){
+    console.log('domain=' + $scope.domainName 
+      + '\nphpversion=' + $scope.php_version  
+      + '\nhttpversion=' + $scope.http_version
+      + '\nmysqlversion=' + $scope.mysql_version);
+    // send install infomation to server
+    // -----------send a request to server and your code here!!!-------------
+    // then redict to 'creating page'
+    $location.path('/sites/creating')
+  }
+
+  }]);
+
+
+cloudAppControllers.controller('siteCreatingCtrl', ['$rootScope','$scope',
+  function($rootScope,$scope) {
+  /* custom code section */
+  $rootScope.htmlTitle = "一键通";
+  
+  }]);
+
+cloudAppControllers.controller('lampCtrl', ['$rootScope','$scope',
+  function($rootScope,$scope) {
+  /* custom code section */
+  $rootScope.htmlTitle = "一键通";
+  
+  }]);
+
+cloudAppControllers.controller('lnmpCtrl', ['$rootScope','$scope',
+  function($rootScope,$scope) {
+  /* custom code section */
+  $rootScope.htmlTitle = "一键通";
+  
+  }]);
